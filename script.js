5f3cea766102bfaaf019c9b8ac7f2aba27fffe45
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-11-09 15:57:39 +0200

    update

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const inputName = document.querySelector('#input__name')
+const screenStart = document.querySelector('#screen__start')
+
+document.querySelector('#btn__play').addEventListener('click', () => {
+  if (inputName.value.trim().length > 0) {
+    alert('start game')
+  } else {
+    inputName.classList.add('input--err');
+    setTimeout(() => {
+      inputName.classList.remove('input--err');
+      inputName.focus();
+    }, 250);
+  }
+});
+
 const getGameInfo = () => JSON.parse(localStorage.getItem('game'));
 
 const init = () => {
